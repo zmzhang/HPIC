6499c4075af97a968d90849fa259c80f64e86e35
wangrong <146118wang@gmail.com>
2018-02-28 21:46:40 +0800

    HPIC

---
+++ hpic.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 07 08:40:23 2017
+
+@author: Wangrong
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 08:18:18 2017
+
+@author: Wangrong
+"""
+
+import time,os
+import numpy as np
+#import pyopenms
+import hdbscan
+from sklearn.preprocessing import minmax_scale
+from retention_time import to_deque
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def readms(input_file):#only 'mzml,mzdata or mzxml' format
+    #ms_format = re.search('\.\w+',input_file)
+    import pyopenms
+    ms_format= os.path.splitext(input_file)[1]
+    #ms_format = ms_format.group()
+    ms_format = ms_format.lower()
+    msdata = pyopenms.MSExperiment()
+    if ms_format == '.mzxml':
+        file =  pyopenms.MzXMLFile()
+    elif ms_format == '.mzml':
+        file =  pyopenms.MzMLFile()
+    elif ms_format =='.mzdata':
+        file =  pyopenms.MzDataFile()
+    else:
+        raise Exception('ERROR: %s is wrong format' % input_file)
+    file.load(r'%s' % input_file,msdata)
+    ms = []
+    intensity = []
+    rt = []
+    for spectrum in msdata:
+        if spectrum.getMSLevel() == 1:
+            rt.append(spectrum.getRT())
+            p_ms = []
+            p_intensity = []
+            for peak in spectrum:
+                if peak.getIntensity()!= 0:
+                    p_ms.append(peak.getMZ())
+                    p_intensity.append(peak.getIntensity())
+            #print len(p_intensity)
+            ms_index = np.argsort(-np.array(p_intensity))
+            ms.append(np.array(p_ms)[ms_index])
+            intensity.append(np.array(p_intensity)[ms_index])
+            #scan+=1        
+    rt1 = np.array(rt)
+    rt_mean_interval = np.mean(np.diff(rt1))
+    #print  rt_mean_interval
+    #rt_mean_interval = np.mean(rt1[1:]-rt1[:-1])
+    #return ms,intensity,rt,scan,rt_max_interval
+    return ms,intensity,rt,rt_mean_interval
+#返回质谱，强度，保留时间，扫描次数，以及保留时间的最大间隔，和平均的时间间隔
+def maxI(intensity):
+    #print intensity[:2]
+    max_i = np.array([intensity_i[0] if len(intensity_i)>0 else 0 for intensity_i in intensity])
+    
+    max_intensity_rt = np.argmax(max_i)
+    max_intensity_intensity = max_i[max_intensity_rt]
+    return max_intensity_rt,max_intensity_intensity
+#返回强度最大值得保留时间，以及质谱索引，和强度值
+#输入质谱，强度，保留时间，质谱宽度，保留时间宽度，以及最高强度的保留时间，和质谱索引，和扫描次数
+def choosedata(ms,intensity,rt,inv_ms,inv_rt,max_intensity_rt,scan):
+    max_int_ms =ms[max_intensity_rt][0]
+    #max_int_ms_intensity = max(max_i)
+    #mz_max = max_int_ms+inv_ms
+    #mz_min = max_int_ms-inv_ms
+    #choose_ms = []
+    #choose_intensity = []
+    start = max_intensity_rt-inv_rt
+    end = max_intensity_rt+inv_rt
+    if start <0:
+        start = 0
+    if end>scan:
+        end = scan
+    choose_spec = []
+    for index in range(start,end):
+        ms_index = np.where(np.abs(ms[index]-max_int_ms)<inv_ms)[0]
+        if ms_index.shape[0]>0:
+            rt_1 = np.full(ms_index.shape[0],rt[index], dtype=np.double)
+            choose_spec.extend(zip(rt_1,ms[index][ms_index],intensity[index][ms_index]))
+        #choose_ms.append(ms[index][ms_index])
+        #choose_intensity.append(intensity[index][ms_index])
+    #print h_intensity_rt
+    choose_spec = np.array(choose_spec)
+    choose_rt = rt[start:end]
+    h_intensity_rt = rt[max_intensity_rt]
+    return choose_spec,choose_rt,max_int_ms,h_intensity_rt
+def hdbscan_lc(choose_spec,choose_rt,h_intensity_rt,max_int_ms,rt_inv,mis_gap):
+    #import hdbscan
+    #from sklearn.preprocessing import minmax_scale
+    #print 'what wrong'
+    choose_spec_trans = np.zeros_like(choose_spec[:,:2])
+    choose_spec_trans[:,1] = np.abs(choose_spec[:,1]-max_int_ms)
+    choose_spec_trans[:,0] = choose_spec[:,0]-h_intensity_rt
+    data = minmax_scale(choose_spec_trans)
+    data[:,1] = data[:,1]*50
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=5)
+    clusterer.fit(data)
+    labels = clusterer.labels_
+    label_index = np.argmax(choose_spec[:,2])
+    label = labels[label_index]
+    if label == -1:
+        choose_spec_1 = choose_spec[label_index].reshape(1,3)        
+    else:
+        choose_spec_1 = choose_spec[labels==label,:]
+        #plt.scatter(choose_spec_1[:,0],choose_spec_1[:,1],marker = 'o', color = 'r')
+        #plt.scatter(choose_spec[labels!=label,:][:,0],choose_spec[labels!=label,:][:,1])
+        #plt.savefig(r'D:/new direction/look/%s_scatter.png' % max_int_ms)
+        #plt.close()
+        choose_spec_1_rt = choose_spec_1[:,0]
+        choose_rt_1 =choose_rt[choose_rt.index(choose_spec_1_rt[0]):choose_rt.index(choose_spec_1_rt[-1])+1]
+        no_common = np.setdiff1d(np.array(choose_rt_1),choose_spec_1_rt)
+        #print no_common.shape[0]
+        if no_common.shape[0]>mis_gap:
+            left = no_common[no_common<h_intensity_rt]
+            if left.shape[0]>mis_gap:
+                pre_rt = np.searchsorted(choose_spec_1_rt,left[-mis_gap-1])
+            else:
+                pre_rt = 0
+            right = no_common[no_common>h_intensity_rt]
+            if right.shape[0]>mis_gap:
+                dif_rt = np.searchsorted(choose_spec_1_rt,right[mis_gap])
+            else:
+                dif_rt = len(choose_spec_1_rt)
+            choose_spec_1 = choose_spec_1[pre_rt:dif_rt,:]
+            choose_spec_1_rt = choose_spec_1[:,0]
+        choose_1_rt = np.diff(choose_spec_1_rt)
+        #delete the data while there are more than one point at the rentention time
+        if not np.all(choose_1_rt):
+            choose_1_rt_index = range(choose_spec_1_rt.shape[0])
+            choose_1_rt = np.where(choose_1_rt ==0)[0]
+            choose_spec_1_rt_1 = choose_spec_1_rt[choose_1_rt]
+            k= 0
+            while k<choose_spec_1_rt_1.shape[0]:
+                p_multiple = choose_spec_1[choose_spec_1_rt==choose_spec_1_rt_1[k]]
+                p_index = range(choose_1_rt[k],choose_1_rt[k]+ p_multiple.shape[0])
+                p_index.remove(np.argmin(np.abs(p_multiple[:,1]-max_int_ms))+choose_1_rt[k])
+                for index in p_index:
+                    choose_1_rt_index.remove(index)
+                    
+                k +=(p_multiple.shape[0]-1)
+            choose_spec_1= choose_spec_1[choose_1_rt_index,:]
+            #plt.scatter(choose_spec_1[:,0],choose_spec_1[:,2],marker = 'o', color = 'r')
+            #plt.savefig(r'D:/new direction/look/%s_peak.png' % max_int_ms)
+            #plt.close()	
+		
+    return choose_spec_1
+def PIC(inputfile,file_t,min_intensity=200,mass_inv=1,rt_inv=50,mis_gap=3):
+    #start = time.time() 
+    ms,intensity,rt,rt_mean_interval=readms(inputfile)
+    scan = len(rt)
+    #print scan
+    while True:
+        max_intensity_rt,max_intensity_intensity = maxI(intensity)
+        if max_intensity_intensity<min_intensity:
+            break
+        else:
+            choose_spec,choose_rt,max_int_ms,h_intensity_rt = choosedata(ms,intensity,rt,mass_inv,rt_inv,max_intensity_rt,scan)
+            if choose_spec.shape[0]<5:
+                choose_spec_2= choose_spec[np.argmax(choose_spec[:,2])].reshape(1,3)
+            else:
+                choose_spec_2 = hdbscan_lc(choose_spec,choose_rt,h_intensity_rt,max_int_ms,rt_inv,mis_gap)
+                #np.savetxt('%s/%s_%s_%s_1.txt' % (file_t,max_int_ms,h_intensity_rt,max_intensity_intensity),choose_spec)
+            np.savetxt('%s/%s_%s_%s_%s_%s_%s.txt' % (file_t,max_int_ms,h_intensity_rt,max_intensity_intensity,choose_spec_2[0,0],choose_spec_2[-1,0],choose_spec_2.shape[0]),choose_spec_2)                
+            del_1_index = rt.index(choose_spec_2[0,0])
+            del_2_index = rt.index(choose_spec_2[-1,0])+1
+            del_not = np.setdiff1d(np.array(rt[del_1_index:del_2_index]),choose_spec_2[:,0])
+            del_all_ms = choose_spec_2[:,1]
+            del_not_count = 0
+            for index,del_ms_index in enumerate(range(del_1_index,del_2_index)):
+                if rt[del_ms_index] in del_not:
+                    del_not_count+=1
+                else:
+                    index = index-del_not_count
+                    intensity[del_ms_index] = intensity[del_ms_index][ms[del_ms_index]!=del_all_ms[index]]
+                    ms[del_ms_index] = ms[del_ms_index][ms[del_ms_index]!=del_all_ms[index]]
+    #end = time.time()
+    #print (end-start)/60
+    return rt_mean_interval
+#file_ms = 'D:/1/plot_0.2'
+#file_dir = 'MM48_MSS.mzML'
+#file_result = '1/test'
+#file_all = os.listdir('%s' % file_dir)
+#print file_all
+'''for i in file_all[6:8]:
+    #print i
+    PIC('%s/%s' % (file_dir,i),os.path.splitext(os.path.basename(i))[0],500)'''
+#EIC(r'D:\data\pos\Mspos_MM48_20uM_1-B,1_01_14615.mzdata',1,50,2,'D:/1/10_50_5_2')
+#PIC('D:/HDBSCAN_code/MM48_MSS.mzML','D:/HDBSCAN_code/2',200)
+def hpic(file_in,file_out,min_intensity=250,min_snr=3):
+    file_t = os.path.splitext(os.path.basename(file_in))[0]
+    file_t = '%s/%s' % (file_out,file_t)
+    try:
+        os.makedirs('%s' % file_t )
+    except:
+        pass
+    interval = PIC(file_in,file_t,min_intensity)*1.5
+    return to_deque(file_t,file_out,min_snr,interval,min_intensity)
+hpic(r'D:/data/pos/Mspos_MM48_20uM_1-B,1_01_14616.mzdata','D:/5/Mspos_MM48_20uM_1-B,1_01_14616',250,3)
+    
+      
+
+    
+     
+
+        
+            
+
+    
+    
+    
+    
+
+
+
+
---
+++ lc_ms_peak.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 05 12:22:39 2017
+
+@author: Wangrong
+"""
+
+from __future__ import division
+ 
+import numpy as np
+from scipy.signal import fftconvolve
+from scipy.stats import scoreatpercentile, mode
+from collections import deque
+from math import ceil
+def mexican_hat(points, a):
+    A = 2 / (np.sqrt(3 * a) * (np.pi ** 0.25))
+    wsq = a ** 2
+    vec = np.arange(0, points) - (points - 1.0) / 2
+    tsq = vec ** 2
+    mod = (1 - tsq / wsq)
+    gauss = np.exp(-tsq / (2 * wsq))
+    total = A * mod * gauss
+    return total
+def cwt(data, wavelet, widths):
+    output = np.zeros([len(widths), len(data)])
+    for ind, width in enumerate(widths):
+        wavelet_data = wavelet(min(10 * width, len(data)), width)
+        output[ind, :] = fftconvolve(data, wavelet_data,
+                                     mode='same')
+    return output
+def local_extreme(data, comparator,
+                  axis=0, order=1, mode='clip'):
+    if (int(order) != order) or (order < 1):
+        raise ValueError('Order must be an int >= 1')
+    datalen = data.shape[axis]
+    locs = np.arange(0, datalen)
+    results = np.ones(data.shape, dtype=bool)
+    main = data.take(locs, axis=axis, mode=mode)
+    for shift in range(1, order + 1):
+        plus = data.take(locs + shift, axis=axis, mode=mode)
+        minus = data.take(locs - shift, axis=axis, mode=mode)
+        results &= comparator(main, plus)
+        results &= comparator(main, minus)
+    return results
+def ridge_detection(local_max, row_best, col, n_rows, n_cols, minus=True, plus=True):
+    cols = deque()
+    rows = deque()
+    cols.append(col)
+    rows.append(row_best)
+    col_plus = col
+    col_minus = col
+    for i in range(1, n_rows):
+        row_plus = row_best + i
+        row_minus = row_best - i
+        segment_plus = 1
+        segment_minus = 1
+        if minus and row_minus > 0 and segment_minus < col_minus < n_cols - segment_minus - 1:
+            if local_max[row_minus, col_minus + 1]:
+                col_minus += 1
+            elif local_max[row_minus, col_minus - 1]:
+                col_minus -= 1
+            elif local_max[row_minus, col_minus]:
+                col_minus = col_minus
+            else:
+                col_minus = -1
+            if col_minus != -1:
+                rows.appendleft(row_minus)
+                cols.appendleft(col_minus)
+        if plus and row_plus < n_rows and segment_plus < col_plus < n_cols - segment_plus - 1:
+            if local_max[row_plus, col_plus + 1]:
+                col_plus += 1
+            elif local_max[row_plus, col_plus - 1]:
+                col_plus -= 1
+            elif local_max[row_plus, col_plus]:
+                col_plus = col_plus
+            else:
+                col_plus = -1
+            if col_plus != -1:
+                rows.append(row_plus)
+                cols.append(col_plus)
+        if (minus and False == plus and col_minus == -1) or \
+                (False == minus and True == plus and col_plus == -1) or \
+                (True == minus and True == plus and col_plus == -1 and col_minus == -1):
+            break
+    #print rows,cols
+    return rows, cols
+def peaks_position(vec, ridges, cwt2d, wnd=2):
+    n_cols = cwt2d.shape[1]
+    negs = cwt2d < 0
+    local_minus = local_extreme(cwt2d, np.less, axis=1, order=1)
+    #zero_crossing = np.abs(np.diff(np.sign(cwt2d))) / 2
+    negs |= local_minus
+    negs[:, [0, n_cols - 1]] = True
+    ridges_select = []
+    peaks = []
+ 
+    for ridge in ridges:
+        inds = np.where(cwt2d[ridge[0, :], ridge[1, :]] > 0)[0]
+        if len(inds) > 0:
+            col = int(mode(ridge[1, inds])[0][0])
+            rows = ridge[0, :][(ridge[1, :] == col)]
+            row = rows[0]
+            cols_start = max(col - np.where(negs[row, 0:col][::-1])[0][0], 0)
+            cols_end = min(col + np.where(negs[row, col:n_cols])[0][0], n_cols)
+            if cols_end > cols_start:
+                inds = range(cols_start, cols_end)
+                peaks.append(inds[np.argmax(vec[inds])])
+                ridges_select.append(ridge)
+        elif ridge.shape[1] > 2: # local wavelet coefficients < 0
+            cols_accurate = ridge[1, 0:int(ceil(ridge.shape[1] / 2))]
+            cols_start = max(np.min(cols_accurate) - 3, 0)
+            cols_end = min(np.max(cols_accurate) + 4, n_cols - 1)
+            inds = range(cols_start, cols_end)
+            if len(inds) > 0:
+                peaks.append(inds[np.argmax(vec[inds])])
+                ridges_select.append(ridge)
+    peaks_1 = np.unique(peaks)
+    if len(peaks)> peaks_1.shape[0]:
+        ridges_len = np.array([ridge.shape[1] for ridge in ridges_select])
+        ridges_refine = []
+        for peak in peaks_1:
+            inds = np.where(peaks == peak)[0]
+            ridge = ridges_select[inds[np.argmax(ridges_len[inds])]]
+            ridges_refine.append(ridge)
+        return list(peaks_1),ridges_refine
+    else:
+        return peaks,ridges_select
+def ridges_detection(cwt2d, vec):
+    n_rows = cwt2d.shape[0]
+    n_cols = cwt2d.shape[1]
+    local_max = local_extreme(cwt2d, np.greater, axis=1, order=1)
+    #print local_max
+    ridges = []
+    rows_init = np.array(range(1, 6))
+    cols_small_peaks = np.where(np.sum(local_max[rows_init, :], axis=0) > 0)[0]
+    for col in cols_small_peaks:
+        best_rows = rows_init[np.where(local_max[rows_init, col])[0]]
+        rows, cols = ridge_detection(local_max, best_rows[0], col, n_rows, n_cols, True, True)
+        staightness = 1 - float(sum(abs(np.diff(cols)))) / float(len(cols))
+        if len(rows) >=4 and staightness >=0 and \
+            not(
+            len(ridges) > 0 and
+            np.array_equal(np.array(cols),ridges[-1][1])
+        ):
+            ridges.append(np.array([rows, cols], dtype=np.int32))
+    return ridges
+def signal_noise_ratio(cwt2d, ridges, peaks):
+    n_cols = cwt2d.shape[1]
+    row_one = cwt2d[0, :]
+    row_one_del = np.delete(row_one, np.where(abs(row_one) < 10e-5))
+    t = 3 * np.median(np.abs(row_one_del - np.median(row_one_del))) / 0.67
+    row_one[row_one > t] = t
+    row_one[row_one < -t] = -t
+    noises = np.zeros(len(peaks))
+    signals = np.zeros(len(peaks))
+    for ind, val in enumerate(peaks):
+        hf_window = ridges[ind].shape[1]
+        window = range(int(max([val - hf_window, 0])), int(min([val + hf_window, n_cols])))
+        noises[ind] = scoreatpercentile(np.abs(row_one[window]), per=90)
+        signals[ind] = np.max(cwt2d[ridges[ind][0, :], ridges[ind][1, :]])
+    return np.abs((signals + np.finfo(float).eps) / (noises + np.finfo(float).eps)), signals
+def peaks_detection(data, scales, min_snr=3,intensity=200):
+    vec = data[:,2]
+    cwt2d = cwt(vec, mexican_hat, scales)
+    ridges = ridges_detection(cwt2d, vec)
+    peaks, ridges = peaks_position(vec, ridges, cwt2d)
+    peak_list = np.zeros((len(peaks),9))
+    peak_list[:,:3],peak_list[:,3],peak_list[:,4],peak_list[:,7],peak_list[:,8]= data[peaks][:,[1,0,2]],data[0,0],data[-1,0],peaks,data.shape[0]
+    peak_list[:,6], peak_list[:,5] = signal_noise_ratio(cwt2d, ridges, peaks)
+    peak_list = peak_list[(peak_list[:,6]>=min_snr)&(peak_list[:,2]>=intensity)]
+    if peak_list.shape[0]>1:
+        peak_list=peak_list[np.argsort(peak_list[:,7])]
+        for i in range(peak_list.shape[0]-1):
+            rt1_index = int(np.argmin(vec[peak_list[i,7]:peak_list[i+1,7]])+peak_list[i,7])
+            peak_list[i,4],peak_list[i+1,3] = data[rt1_index,0],data[rt1_index,0] 
+    return peak_list
+def lc_ms_peak(data,scales,min_snr,data_p,intensity):
+    if data_p:
+        peak_list = peaks_detection(data, scales, min_snr,intensity)
+    else:
+        data = np.loadtxt(r'%s' % data)
+        peak_list =  peaks_detection(data, scales, min_snr,intensity)
+    return  list(peak_list)
+
+        
+            
+            
+                
+                
+            
+
