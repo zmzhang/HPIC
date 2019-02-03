ed876e380f0ac62c13ecd10acbb931bc190f7b59
zmzhang <zhangzhimin.csu@gmail.com>
2019-02-03 16:18:24 +0800

    download the test dataset by wget for hpic_test

--- hpic/tests/test_hpic.py
+++ hpic/tests/test_hpic.py
@@ -2,14 +2,34 @@
 python -m unittest -v hpic.tests.hpic_test
 """
 
-import unittest
+import unittest, os, wget, tarfile
 from hpic.hpic import hpic
+
+def makedir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 class hpic_test(unittest.TestCase):
 
+    def setUp(self):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        dir_data = os.path.join(dirname, "tmp/data")
+        dir_result = os.path.join(dirname, "tmp/result")
+        tar_path = os.path.join(dir_data, "Sample-1.tar.bz2")
+        self.data_path = os.path.join(dir_data, "MM14_20um.mzxml")
+        self.dir_result = dir_result
+        if not(os.path.isfile(self.data_path)):
+            makedir(dir_data)
+            makedir(dir_result)
+            wget.download(url="https://msbi.ipb-halle.de/download/Sample-1.tar.bz2", out=dir_data)
+            tar = tarfile.open(tar_path, "r:bz2")
+            tar.extractall(dir_data)
+            tar.close()
+
     def test_hpic(self):
-        peak_list = hpic('D:/data/Mspos_MM48_20uM_1-B,1_01_14614.mzxml','D:/data/result',250,3)
+        peak_list = hpic(self.data_path, self.dir_result, 250, 3)
         peak_list_sorted = peak_list.sort_values(by='intensity', ascending=False)
-        self.assertAlmostEqual(peak_list_sorted.iloc[0]['intensity'], 654353)
+        self.assertAlmostEqual(peak_list_sorted.iloc[0]['intensity'], 208942.0)
 
 if __name__ == '__main__':
     unittest.main()
