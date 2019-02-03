f0f13c71df157713e611aad7c8e616e4bd0bed1a
zmzhang <zhangzhimin.csu@gmail.com>
2019-02-03 14:11:53 +0800

    assert for hpic_test

--- hpic/tests/test_hpic.py
+++ hpic/tests/test_hpic.py
@@ -7,8 +7,9 @@ from hpic.hpic import hpic
 class hpic_test(unittest.TestCase):
 
     def test_hpic(self):
-        hpic(r'D:/data/Mspos_MM48_20uM_1-B,1_01_14614.mzxml','D:/data/result',250,3)
-        self.assertEqual(1, 1)
+        peak_list = hpic('D:/data/Mspos_MM48_20uM_1-B,1_01_14614.mzxml','D:/data/result',250,3)
+        peak_list_sorted = peak_list.sort_values(by='intensity', ascending=False)
+        self.assertAlmostEqual(peak_list_sorted.iloc[0]['intensity'], 654353)
 
 if __name__ == '__main__':
     unittest.main()
